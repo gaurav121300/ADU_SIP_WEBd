a76d0cba69b47a70170dcf964c09eeb66bb0fd31
gaurav121300 <143287813+gaurav121300@users.noreply.github.com>
2026-06-02 16:37:02 +0530

    Add files via upload

---
+++ ADU_SIP_WEBd/Lecture_22/script.js
@@ -0,0 +1,83 @@
+console.log("Hello Everyone")
+
+// if(true){
+//     var age = 31;
+// }
+
+// console.log(age);  // I can access age as it is a var type
+
+// if(true){
+//     let age = 20;  //scope is only valid till here not outside of this
+// }
+
+// console.log(age)  //can not access
+
+// let age = 20;
+// console.log(age);
+
+// let name = "ADU"
+// console.log(name);
+
+// let isStudent = true;
+// console.log(isStudent);
+
+// let x;
+// console.log(x);
+
+// let y = null;
+// console.log(y);
+
+// console.log(typeof isStudent);  //ans is boolean
+
+// //operators
+
+// let a = 20;
+// let b = 10;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a *b);
+// console.log(a/b);
+// console.log(a % b);
+
+// //Assignment operator
+// a = a + 5;  // one more way a += 5;
+// console.log(a);  //a become 25
+
+
+// console.log(true && false);  //false
+// console.log(true || false);  //true
+// console.log(!true);
+
+//Type Casting
+// let iniNum = "100";
+// console.log(typeof iniNum);
+
+
+// let num = Number(iniNum);
+
+// console.log(num);
+// console.log(typeof num);
+
+//Number to String
+
+// let num = 100;
+// let str = String(num);
+
+// console.log(typeof str);
+
+// console.log(Boolean(1));
+
+// console.log(Boolean(0));
+
+// console.log(Number("123"));  
+// console.log(Number("Hello"));  //NaN not a number
+
+let a = "123";
+let b = 5;
+
+console.log(a + b)
+
+console.log(Number(a) + b);
+
+
